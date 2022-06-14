6b5eb08583a355c538baa43e019bf9ce326b3817
tuhuynh <tuhuynh1902@gmail.com>
2022-06-15 03:17:44 +0700

    #6.0 ỎM

--- jwtBackend/src/controller/homeController.js
+++ jwtBackend/src/controller/homeController.js
@@ -6,7 +6,6 @@ const handleHelloWorld = (req, res) => {
 
 const handleUserPage = async (req, res) => {
   let userList = await userService.getUserList();
-  await userService.deleteUser(10);
   return res.render('user.ejs', { userList });
 };
 
