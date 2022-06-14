a46af954554fe2b4fcfca84df4e19b5dc28164e8
tuhuynh <tuhuynh1902@gmail.com>
2022-06-15 00:51:44 +0700

    #5.2 Read User with NODEJS

--- jwtBackend/src/service/userService.js
+++ jwtBackend/src/service/userService.js
@@ -1,12 +1,8 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import bcrypt from 'bcrypt';
+import bluebird from 'bluebird';
 
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'jwt',
-});
+// create the connection, specify bluebird as Promise
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -29,13 +25,29 @@ const createNewUser = (email, password, username) => {
   );
 };
 
-const getUserList = () => {
-  let users = [];
-  connection.query('Select * from users', function (err, results, fields) {
-    if ((err, results, fields)) {
-      console.log(err);
-    }
+const getUserList = async () => {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt',
+    Promise: bluebird,
   });
+  // let users = [];
+  // return connection.query('Select * from users', function (err, results, fields) {
+  //   if (err) {
+  //     console.log(err);
+  //     return users;
+  //   }
+  //   users = results;
+  //   console.log('> run get user list', users);
+  //   return users;
+  // });
+  try {
+    const [rows, fields] = await connection.execute('Select * from users');
+    return rows;
+  } catch (error) {
+    console.log('check error', error);
+  }
 };
 
 module.exports = { createNewUser, getUserList };
