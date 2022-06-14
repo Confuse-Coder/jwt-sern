304c20426dadddd9cb0b91cc1c21ea59d5eb9387
tuhuynh <tuhuynh1902@gmail.com>
2022-06-15 02:38:40 +0700

    #5.4 Update User with NODEJS

--- jwtBackend/src/service/userService.js
+++ jwtBackend/src/service/userService.js
@@ -61,4 +61,40 @@ const deleteUser = async (id) => {
   }
 };
 
-module.exports = { createNewUser, getUserList, deleteUser };
+const getUserById = async (id) => {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt',
+    Promise: bluebird,
+  });
+
+  try {
+    const [rows, fields] = await connection.execute('select * from users where id=?', [id]);
+    return rows;
+  } catch (error) {
+    console.log('check error', error);
+  }
+};
+
+const updateUserInfor = async (email, username, id) => {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt',
+    Promise: bluebird,
+  });
+
+  try {
+    const [rows, fields] = await connection.execute(
+      'UPDATE users SET email=?, username = ? WHERE id = ?',
+      [email, username, id]
+    );
+    console.log('check row >>>', rows);
+    return rows;
+  } catch (error) {
+    console.log('check error', error);
+  }
+};
+
+module.exports = { createNewUser, getUserList, deleteUser, getUserById, updateUserInfor };
