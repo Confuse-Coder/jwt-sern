65453d04a52d1ed17542155a3d61f69cadef0aa1
tuhuynh <tuhuynh1902@gmail.com>
2022-06-14 19:21:43 +0700

    #6

---
+++ jwtBackend/src/service/userService.js
@@ -0,0 +1,41 @@
+import mysql from 'mysql2';
+import bcrypt from 'bcrypt';
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'jwt',
+});
+
+const salt = bcrypt.genSaltSync(10);
+
+const hashUserPassword = (userPassword) => {
+  let hashPassword = bcrypt.hashSync(userPassword, salt);
+  return hashPassword;
+};
+
+const createNewUser = (email, password, username) => {
+  let hashPass = hashUserPassword(password);
+
+  connection.query(
+    'INSERT INTO users (email, password, username) VALUES (?,?,?)',
+    [email, hashPass, username],
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+};
+
+const getUserList = () => {
+  let users = [];
+  connection.query('Select * from users', function (err, results, fields) {
+    if ((err, results, fields)) {
+      console.log(err);
+    }
+  });
+};
+
+module.exports = { createNewUser, getUserList };
