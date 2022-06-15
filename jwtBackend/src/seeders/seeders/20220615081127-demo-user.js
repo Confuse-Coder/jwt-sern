260c0bd2bdf0c34be3fbc49a31819db8d2f69456
tuhuynh <tuhuynh1902@gmail.com>
2022-06-15 15:20:15 +0700

    #6.1 setting up seed-model-migration

---
+++ jwtBackend/src/seeders/seeders/20220615081127-demo-user.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          email: 'John Doe',
+          password: '123',
+          username: 'fake',
+        },
+        {
+          email: 'John ',
+          password: '123',
+          username: 'fake fake',
+        },
+        {
+          email: ' Doe',
+          password: '123',
+          username: 'fake fake fake',
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
--- jwtBackend/src/server.js
+++ jwtBackend/src/server.js
@@ -2,17 +2,21 @@ import express from 'express';
 import configViewEngine from './config/viewEngine';
 import initWebRoutes from './routes/web';
 import bodyParser from 'body-parser';
+require('dotenv').config();
+// import connection from './config/connectDB';
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 configViewEngine(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-initWebRoutes(app);
+//test connection DB
+// connection();
 
-const PORT = 8080;
+initWebRoutes(app);
 
 app.listen(PORT, () => {
   console.log('>>> JWT BackEnd is running on PORT =' + PORT);
