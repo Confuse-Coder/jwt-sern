a4eb51979227527686fb4bd7f151e0a40f48390b
tuhuynh <tuhuynh1902@gmail.com>
2022-06-16 10:35:12 +0700

    React-Router

---
+++ jwtFrontend/src/components/Login/Login.js
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <div className="login-container">
+      Login
+      <br />
+      <button className="btn btn-secondary">Button</button>
+    </div>
+  );
+};
+
+export default Login;
