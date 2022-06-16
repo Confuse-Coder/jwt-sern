78daf591a1be1b20034413980ae56f326862cbf5
tuhuynh <tuhuynh1902@gmail.com>
2022-06-16 18:36:03 +0700

    #10.2.1 API Register

--- jwtBackend/src/controller/apiController.js
+++ jwtBackend/src/controller/apiController.js
@@ -1,5 +1,33 @@
-const handleRegister = (req, res) => {
-  console.log('>>Call me?', req.body);
+import loginRegisterService from '../service/loginRegisterService';
+
+const handleRegister = async (req, res) => {
+  try {
+    if (!req.body.email || !req.body.phone || !req.body.password) {
+      return res.status(200).json({
+        EM: 'Missing required parameters!',
+        EC: 1,
+      });
+    }
+    if (req.body.password && req.body.password < 4) {
+      return res.status(200).json({
+        EM: 'Your password must have more than 3 letters!',
+        EC: 1,
+      });
+    }
+
+    let data = await loginRegisterService.registerNewUser(req.body);
+
+    return res.status(200).json({
+      EM: data.EM,
+      EC: data.EC,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      EM: 'error from server!',
+      EC: -1,
+    });
+  }
 };
 
 module.exports = { handleRegister };
