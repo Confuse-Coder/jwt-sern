e3b7844b1414f5d1fa44508468cb7c916aa1a809
tuhuynh <tuhuynh1902@gmail.com>
2022-06-17 20:01:12 +0700

    #10.6.3 Delete Users

--- jwtBackend/src/controller/userController.js
+++ jwtBackend/src/controller/userController.js
@@ -56,7 +56,12 @@ const updateFunc = async (req, res) => {
 };
 const deleteFunc = async (req, res) => {
   try {
-    let users = await userApiService.getAllUsers();
+    let data = await userApiService.deleteUser(req.body.id);
+    return res.status(200).json({
+      EM: data.EM,
+      EC: data.EC,
+      DT: data.DT,
+    });
   } catch (e) {
     console.log(e);
     return res.status(500).json({
