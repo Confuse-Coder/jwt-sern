3752158cea520d199cdaf258ada9f9e981bf47c0
tuhuynh <tuhuynh1902@gmail.com>
2022-06-18 16:54:42 +0700

    JWT Token Begin

--- jwtBackend/src/server.js
+++ jwtBackend/src/server.js
@@ -5,6 +5,7 @@ import initApiRoutes from './routes/api';
 import bodyParser from 'body-parser';
 import configCors from './config/cors';
 require('dotenv').config();
+import { createJWT, verifyToken } from './middleware/JWTActions';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -19,6 +20,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //test connection DB
 // connection();
+
+//test JWT
+createJWT();
+let decodedData = verifyToken(
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVG9vIiwiYWRkcmVzcyI6IkhDTSIsImlhdCI6MTY1NTU0NTQ1NX0.cCvMko1eI6I5AigBCgGoDg4UYwGeJmMFjc12yz-xwLM'
+);
+console.log(decodedData);
 
 initWebRoutes(app);
 initApiRoutes(app);
