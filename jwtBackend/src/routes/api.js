4c7c27aef82c28e27aeed68ac27d76c2b0a72e59
tuhuynh <tuhuynh1902@gmail.com>
2022-06-19 18:31:30 +0700

    #11.6 JWT Middleware

--- jwtBackend/src/routes/api.js
+++ jwtBackend/src/routes/api.js
@@ -2,6 +2,7 @@ import express from 'express';
 import apiController from '../controller/apiController';
 import userController from '../controller/userController';
 import groupController from '../controller/groupController';
+import { checkUserJWT, checkUserPermission } from '../middleware/JWTActions';
 
 const router = express.Router();
 
@@ -10,11 +11,22 @@ const testMiddleware = (req, res, next) => {
   next();
 };
 
+// const checkUserLogin = (req, res, next) => {
+//   const nonSecurePaths = ['/', '/register', '/login'];
+//   if (nonSecurePaths.includes(req.path)) return next();
+
+//   //authenticate user
+//   if (user) {
+//     next();
+//   } else {
+//   }
+// };
+
 const initApiRoutes = (app) => {
   router.post('/register', apiController.handleRegister);
   router.post('/login', apiController.handleLogin);
 
-  router.get('/user/read', userController.readFunc);
+  router.get('/user/read', checkUserJWT, checkUserPermission, userController.readFunc);
   router.post('/user/create', userController.createFunc);
   router.put('/user/update', userController.updateFunc);
   router.delete('/user/delete', userController.deleteFunc);
