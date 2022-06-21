15a48263e7e490a99fd9317d5d9e20ae1a547f8f
tuhuynh <tuhuynh1902@gmail.com>
2022-06-21 19:18:26 +0700

    #11.11 Bearer Authentication

--- jwtBackend/src/middleware/JWTActions.js
+++ jwtBackend/src/middleware/JWTActions.js
@@ -21,19 +21,28 @@ const verifyToken = (token) => {
   let decoded = null;
   try {
     decoded = jwt.verify(token, key);
-    console.log(decoded);
   } catch (err) {
     console.log(err);
   }
   return decoded;
 };
 
+const extractToken = (req) => {
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    return req.headers.authorization.split(' ')[1];
+  }
+  return null;
+};
+
 const checkUserJWT = (req, res, next) => {
   if (nonSecurePaths.includes(req.path)) return next();
 
   let cookies = req.cookies;
-  if (cookies && cookies.jwt) {
-    let token = cookies.jwt;
+  const tokenFromHeader = extractToken(req);
+
+  if ((cookies && cookies.jwt) || tokenFromHeader) {
+    let token = cookies && cookies.jwt ? cookies.jwt : tokenFromHeader;
+    console.log('cookies', cookies);
     let decoded = verifyToken(token);
 
     if (decoded) {
--- jwtFrontend/src/components/Login/Login.js
+++ jwtFrontend/src/components/Login/Login.js
@@ -50,6 +50,7 @@ const Login = (props) => {
         account: { groupWithRoles, email, username },
       };
 
+      localStorage.setItem('jwt', token);
       loginContext(data);
 
       history.push('/users');
